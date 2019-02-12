2b6b52382fb731bf4a26e0a1b316562c1a9cfee8
isaac israel <email.iisrael@gmail.com>
2019-02-12 11:51:54 -0200

    Update tasks controller with CRUD actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,32 @@
+# frozen_string_literal: true
+
+# Controller Task
 class TasksController < ApplicationController
+  def index
+    raise
+  end
+
+  def show
+    raise
+  end
+
+  def new
+    raise
+  end
+
+  def create
+    raise
+  end
+
+  def edit
+    raise
+  end
+
+  def update
+    raise
+  end
+
+  def destroy
+    raise
+  end
 end
